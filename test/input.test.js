dffb4fb97db8bbb0bcba2b68c8fa9fb0e23d7c11
Ciara Bujanos <c.buj22@gmail.com>
2017-06-06 10:24:33 -0600

    some testing

--- test/input.test.js
+++ test/input.test.js
@@ -6,11 +6,42 @@ describe('weather tdd', () => {
   it('it should execute passed in function when clicked', () => {
     const mockFn = jest.fn();
     const component = shallow(<Input inputHandle={mockFn} />)
-
     var button = component.find('button');
-    console.log(button);
+
     button.simulate('click');
 
     expect(mockFn).toHaveBeenCalledTimes(1);
   })
+
+  it('should update state on the input with the value in input', () => {
+    const component = mount(<Input />)
+    var input = component.find('input[type="text"]');
+    var inputVal = 'Denver, Co';
+    var changeEvent = { target: { value: inputVal } };
+
+    input.simulate('change', changeEvent);
+
+    expect(input.prop('value')).toEqual(inputVal);
+    expect(component.state('input')).toEqual(inputVal);
+
+  })
+
+  it.skip ('should clear the input field on click', () => {
+    // const component = mount(<Input />)
+    // var input = component.find('input[type="text"]');
+    // var inputVal = 'Denver, Co';
+    // var changeEvent = { target: { value: inputVal } };
+    //
+    // input.simulate('change', changeEvent);
+    // var button = component.find('.input1-btn');
+    // console.log(button);
+    // button.simulate('click');
+    //
+    // expect(input.prop('value')).toEqual('')
+
+    // expect(component.state('input')).toEqual('');
+  })
+
+  
+
 })
